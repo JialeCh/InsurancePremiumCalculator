414d024400d7a760a829f21159735f0280a71ac6
taruc <chengjl-wm17@student.tarc.edu.my>
2019-12-09 12:58:34 +0800

    init

---
+++ app/src/main/java/com/example/insurancepremiumcalculator/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.insurancepremiumcalculator
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var premium :PremiumModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        premium = ViewModelProviders.of(this).
+            get(PremiumModel ::class.java)
+        findViewById<Button>(R.id.Calculate).setOnClickListener {
+            totalPremium.text= calculation().toString()
+        }
+
+        findViewById<Button>(R.id.Reset).setOnClickListener {
+         AgeRange.setSelection(0)
+            Gender.clearCheck()
+            Smoker.isChecked =false
+            totalPremium.text= " "
+        }
+    }
+   fun calculation():Double{
+
+        val age = AgeRange.selectedItemPosition
+
+         return when (age){
+            0 -> 60.00
+            1 -> 70.00 +
+                      (if(male.isChecked) 50.00 else 0.0) +
+                      (if(Smoker.isChecked) 100.00 else 0.0)
+            2 ->  90.00 +
+                    (if(male.isChecked) 100.00 else 0.0) +
+                    (if(Smoker.isChecked) 150.00 else 0.0)
+            3 -> 120.00 +
+                    (if(male.isChecked) 150.00 else 0.0) +
+                    (if(Smoker.isChecked) 200.00 else 0.0)
+            4 -> 150.00 +
+                    (if(male.isChecked) 200.00 else 0.0) +
+                    (if(Smoker.isChecked) 250.00 else 0.0)
+            5 -> 150.00 +
+                    (if(male.isChecked) 250.00 else 0.0) +
+                    (if(Smoker.isChecked) 300.00 else 0.0)
+            else -> 0.0
+
+        }
+
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/insurancepremiumcalculator/PremiumModel.kt
@@ -0,0 +1,22 @@
+package com.example.insurancepremiumcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class PremiumModel : AppCompatActivity() {
+
+  var
+
+    }
+
+
+
+
+
+}
