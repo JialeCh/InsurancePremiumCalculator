18fe3f0addb5bcd38aca28c96f5d3345b0553b6d
JaydenCh <chengjl-wm17@student.tarc.edu.my>
2019-12-14 22:28:55 +0800

    init

--- app/src/main/java/com/example/insurancepremiumcalculator/MainActivity.kt
+++ app/src/main/java/com/example/insurancepremiumcalculator/MainActivity.kt
@@ -14,17 +14,22 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var premium :PremiumModel
+    lateinit var premiumModel: PremiumModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        premium = ViewModelProviders.of(this).
-            get(PremiumModel ::class.java)
+        premiumModel = ViewModelProviders.of(this).get(PremiumModel:: class.java)
+
+
         findViewById<Button>(R.id.Calculate).setOnClickListener {
-            totalPremium.text= calculation().toString()
+            premiumModel.premium= calculation()
+            totalPremium.text = calculation().toString()
+
         }
 
+
+        display()
         findViewById<Button>(R.id.Reset).setOnClickListener {
          AgeRange.setSelection(0)
             Gender.clearCheck()
@@ -32,30 +37,35 @@ class MainActivity : AppCompatActivity() {
             totalPremium.text= " "
         }
     }
+    fun display(){
+        totalPremium.text = calculation().toString()
+    }
    fun calculation():Double{
 
         val age = AgeRange.selectedItemPosition
+if (male.isChecked || Female.isChecked ) {
+    return when (age) {
+        0 -> 60.00
+        1 -> 70.00 +
+                (if (male.isChecked) 50.00 else 0.0) +
+                (if (Smoker.isChecked) 100.00 else 0.0)
+        2 -> 90.00 +
+                (if (male.isChecked) 100.00 else 0.0) +
+                (if (Smoker.isChecked) 150.00 else 0.0)
+        3 -> 120.00 +
+                (if (male.isChecked) 150.00 else 0.0) +
+                (if (Smoker.isChecked) 200.00 else 0.0)
+        4 -> 150.00 +
+                (if (male.isChecked) 200.00 else 0.0) +
+                (if (Smoker.isChecked) 250.00 else 0.0)
+        5 -> 150.00 +
+                (if (male.isChecked) 250.00 else 0.0) +
+                (if (Smoker.isChecked) 300.00 else 0.0)
+        else -> 0.0
 
-         return when (age){
-            0 -> 60.00
-            1 -> 70.00 +
-                      (if(male.isChecked) 50.00 else 0.0) +
-                      (if(Smoker.isChecked) 100.00 else 0.0)
-            2 ->  90.00 +
-                    (if(male.isChecked) 100.00 else 0.0) +
-                    (if(Smoker.isChecked) 150.00 else 0.0)
-            3 -> 120.00 +
-                    (if(male.isChecked) 150.00 else 0.0) +
-                    (if(Smoker.isChecked) 200.00 else 0.0)
-            4 -> 150.00 +
-                    (if(male.isChecked) 200.00 else 0.0) +
-                    (if(Smoker.isChecked) 250.00 else 0.0)
-            5 -> 150.00 +
-                    (if(male.isChecked) 250.00 else 0.0) +
-                    (if(Smoker.isChecked) 300.00 else 0.0)
-            else -> 0.0
-
-        }
+    }
+}else
+    return 0.0
 
     }
 
